b3e4ec5134cac04b0c6d16987edf09c84b4de5ed
caleb-chong <cgc115@georgetown.edu>
2024-11-26 11:59:41 -0500

    Flash Flash Hundred-Yard Dash (Flash messaging)

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,9 +1,12 @@
 # this is the "web_app/__init__.py" file...
 
+import os
 from flask import Flask
 
 from web_app.routes.home_routes import home_routes
 from web_app.routes.stocks_routes import stocks_routes
+
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 
 def create_app():
     app = Flask(__name__)
